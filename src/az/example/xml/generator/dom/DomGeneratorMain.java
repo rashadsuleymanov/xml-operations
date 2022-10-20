9aef60152338b63729651adeb7dc8cc7905749d2
Rashad Suleymanov <resad.suleymanov.8855@gmail.com>
2022-10-20 10:52:55 +0400

    DomGeneratorMain modify

--- src/az/example/xml/generator/dom/DomGeneratorMain.java
+++ src/az/example/xml/generator/dom/DomGeneratorMain.java
@@ -6,10 +6,9 @@ import org.w3c.dom.*;
 
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import java.io.*;
 import java.util.*;
 
 public class DomGeneratorMain {
@@ -66,6 +65,5 @@ public class DomGeneratorMain {
         } catch (ParserConfigurationException | FileNotFoundException | TransformerException e) {
             e.printStackTrace();
         }
-
     }
 }
